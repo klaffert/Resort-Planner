187423f19e12b4979bf7ab0f0556f31d5ac7e212
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-07-29 22:57:00 -0700

    second update

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,21 @@
 class ReservationsController < ApplicationController
+    def new
+        @reservation = Reservation.new
+    end
+
+    def create
+        @reservation = Reservation.create(reservation_params)
+        redirect_to @reservation
+    end
+
+    def show 
+        @reservation= Reservation.find(params[:id])
+    end
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:length, :party_size)
+    end
+   
 end
