bbc888d7208f380ac15397dc7162c963740c5a6a
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-08-02 09:27:17 -0700

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/login', to: "auth#login", as: "login_path"
   post '/login', to: "auth#verify"
   get '/logout', to: "auth#logout", as: "logout"
-  post '/resorts/search_by_city', to: "resorts#search_by_city", as: "resorts_search_by_city"
   post '/resorts/search_by_budget', to: "resorts#search_by_budget", as: "resorts_search_by_budget"
+  post '/resorts/search_by_city', to: "resorts#search_by_city", as: "resorts_search_by_city"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
