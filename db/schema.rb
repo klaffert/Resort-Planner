ba61ebac449a6cf7b896159bac948d0fccb4dac2
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2019-12-09 16:12:21 -0800

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_31_201412) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.integer "length"
