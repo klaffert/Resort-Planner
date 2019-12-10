8dd130a53449ad34c2a6f1768624ec5097e4fcf9
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2019-12-09 16:16:21 -0800

    precomplied

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module HotelBookingApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.initialize_on_precompile = false
   end
 end
